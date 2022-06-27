4067b4c7e9c6c0eefbbafef126e1e90bc44165b2
Laurent Jouanneau <dev@ljouanneau.com>
2022-06-27 17:58:23 +0200

    [tests] fix docker configuration to test against ldaps/starttls on debian

--- tests/unittests/bootstrap.php
+++ tests/unittests/bootstrap.php
@@ -9,7 +9,7 @@ define('TESTAPP_URL_HOST_PORT', 'ldapdao.local');
 define('TESTAPP_HOST', 'ldapdao.local');
 define('TESTAPP_PORT', '');
 
-define('TESTAPP_LDAP_HOST', 'openldap');
+define('TESTAPP_LDAP_HOST', 'ldap.jelix');
 
 
 jApp::setEnv('jelixtests');
